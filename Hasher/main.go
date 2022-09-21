b8c9557c642e1a6945c7cfa14727d0b3c162ee72
Ella Rashty <48799296+EllaRashty@users.noreply.github.com>
2022-09-21 20:04:41 +0300

    cleaning

--- Hasher/main.go
+++ Hasher/main.go
@@ -13,20 +13,19 @@ import (
 
 var client *http.Client
 
-var str string
-
-func Hashing(temp string) string {
-	data := []byte(temp)
+func Hashing(str string) string {
+	data := []byte(str)
 	hash := base64.StdEncoding.EncodeToString(data)
 	return hash
 }
 
-func putHash() http.HandlerFunc {
+func getSha() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// var str string
-		resp, _ := client.Get("http://host.docker.internal:9091/hasher")
+		var str string
+		resp, _ := client.Get("http://host.docker.internal:9091/hash-path")
 		defer resp.Body.Close()
 		json.NewDecoder(resp.Body).Decode(&str)
+		// fmt.Fprintf(w, "str: %s\n", str)
 		hash := Hashing(str)
 		fmt.Fprintf(w, "Hash: %s", hash)
 	}
@@ -40,7 +39,7 @@ func handleRequests() {
 	client = &http.Client{Timeout: 10 * time.Second}
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/hashing", putHash()).Methods("GET")
+	myRouter.HandleFunc("/hashing-service", getSha()).Methods("GET")
 	myRouter.HandleFunc("/", start)
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
@@ -48,54 +47,3 @@ func handleRequests() {
 func main() {
 	handleRequests()
 }
-
-// package main
-
-// import (
-// 	"encoding/base64"
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"time"
-
-// 	"github.com/gorilla/mux"
-// )
-
-// type Str struct {
-// 	Name string `json:"name"`
-// }
-
-// var client *http.Client
-
-// func Hashing(str string) string {
-// 	data := []byte(str)
-// 	hash := base64.StdEncoding.EncodeToString(data)
-// 	return hash
-// }
-
-// func putHash() string {
-// 	var str string
-// 	resp, _ := client.Get("http://localhost:9091/hasher")
-// 	defer resp.Body.Close()
-// 	json.NewDecoder(resp.Body).Decode(&str)
-// 	return Hashing(str)
-// }
-
-// func start(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Port Repository - 9092:8081\n")
-// 	// putHash()
-// 	fmt.Printf("fact: %s\n", putHash())
-// }
-
-// func handleRequests() {
-// 	client = &http.Client{Timeout: 10 * time.Second}
-// 	myRouter := mux.NewRouter().StrictSlash(true)
-
-// 	myRouter.HandleFunc("/", start)
-// 	log.Fatal(http.ListenAndServe(":8081", myRouter))
-// }
-
-// func main() {
-// 	handleRequests()
-// }
--- Repository/main.go
+++ Repository/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -14,34 +15,29 @@ import (
 var client *http.Client
 
 type ContentFact struct {
-	Name string `json:"name"`
-	Path string `json:"path"`
-	Size int    `json:"size"`
-	Sha  string `json:"sha"`
-}
-
-type Item struct {
-	Name string `json:"name"`
-}
-
-type Strs struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	Name     string `json:"name"`
+	Path     string `json:"path"`
+	Size     int    `json:"size"`
+	Sha      string `json:"sha"`
+	Url      string `json:"url"`
+	Type     string `json:"type"`
+	Html     string `json:"html_url"`
+	Download string `json:"download_url"`
 }
 
 type ContentsFacts []ContentFact
 
 var ref = "master"
-var currentPath Item
+var currentPath string
 var strToHash = ""
 
 func GetFileContents(pathInRepo string) ContentsFacts {
-	// if strings.Contains(pathInRepo, "users")
 	var contentsFacts ContentsFacts
-	err2 := GetJson("https://api.github.com/repos/"+pathInRepo+"/contents/?ref="+ref, &contentsFacts)
-	if err2 != nil {
-		fmt.Printf("error getting user fact: %s\n", err2.Error())
+	err := GetJson("https://api.github.com/repos/"+pathInRepo+"/contents/?ref="+ref, &contentsFacts)
+	if err != nil {
+		fmt.Printf("error getting contents facts: %s\n", err.Error())
 	}
+	fmt.Printf("contents of: %s\n", pathInRepo)
 	return contentsFacts
 }
 
@@ -52,8 +48,15 @@ func getPathsForHash() http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		parts := strings.SplitAfter(path, ",")
+		// arr := strings.FieldsFunc(path, func(r rune) bool {
+		// 	return r == ','
+		// })
+		for _, x := range parts {
+			fmt.Fprintf(w, "%s\n", x)
+		}
 		HashFiles(path)
-		fmt.Printf("user: %s\n", path)
+		fmt.Fprintf(w, "The Path For Hashing: %s\n", parts)
 	}
 }
 
@@ -65,6 +68,7 @@ func HashFiles(pathInRepos ...string) {
 		// make sure the hashing will run in the same order no matter to the inputs order
 		sort.Strings(pathInRepos)
 		for _, pathInRepo := range pathInRepos {
+			fmt.Printf("yey? :%s", pathInRepo)
 			allSha = allSha + collectSha(pathInRepo)
 		}
 	}
@@ -82,16 +86,14 @@ func collectSha(pathInRepo string) string {
 
 func CheckoutRef() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var i Item
+		var i string
 		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		ref = i.Name
-		contentsFacts := GetFileContents(currentPath.Name)
-		for i, x := range contentsFacts {
-			fmt.Fprintf(w, "%d.\n name:%s, path:%s,\n sha:%s, size:%d \n", i+1, x.Name, x.Path, x.Sha, x.Size)
-		}
+		ref = i
+		GetFileContents(currentPath)
+		fmt.Fprintf(w, "The Current gitRef is: %s", ref)
 	}
 }
 
@@ -110,9 +112,10 @@ func setPath() http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		contentsFacts := GetFileContents(currentPath.Name)
+		contentsFacts := GetFileContents(currentPath)
 		for i, x := range contentsFacts {
-			fmt.Fprintf(w, "%d.\n name:%s, path:%s,\n sha:%s, size:%d \n", i+1, x.Name, x.Path, x.Sha, x.Size)
+			fmt.Fprintf(w, "%d.\nName: %s,\nPath: %s,\nSha: %s,\nSize: %d,\nUrl: %s,\nType: %s,\nHtml: %s,\nDownload: %s,\n\n", i+1, x.Name, x.Path, x.Sha, x.Size,
+				x.Url, x.Type, x.Html, x.Download)
 		}
 	}
 }
@@ -147,8 +150,8 @@ func handleRequests() {
 	myRouter.HandleFunc("/input-path", setPath()).Methods("POST")
 	myRouter.HandleFunc("/checkout", CheckoutRef()).Methods("POST")
 	myRouter.HandleFunc("/input-path", getCurrentPath()).Methods("GET")
-	myRouter.HandleFunc("/hasher", getHash()).Methods("GET")
-	myRouter.HandleFunc("/hasher", getPathsForHash()).Methods("POST")
+	myRouter.HandleFunc("/hash-path", getHash()).Methods("GET")
+	myRouter.HandleFunc("/hash-path", getPathsForHash()).Methods("POST")
 	myRouter.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
